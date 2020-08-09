ab041a18d0059745e62c6fabc69397d65f018655
kanahashimoto03 <hka13ba077@icloud.com>
2020-08-09 18:29:14 +0900

    rubocopによるコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :family_name, :last_name, :family_name_reading, :last_name_reading, :birthday, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, format: {with: PASSWORD_REGEX, message: "Include both letters and numbers"}
-  
-  validates :family_name, format: {with: /\A[あ-んア-ン一-龥]+\z/, message: "Full-width characters"}
-  validates :last_name, format: {with: /\A[あ-んア-ン一-龥]+\z/, message: "Full-width characters"}
-  validates :family_name_reading, format: {with: /\A[ア-ン]+\z/, message: "Full-width katakana characters"}
-  validates :last_name_reading, format: {with: /\A[ア-ン]+\z/, message: "Full-width katakana characters"}
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
 
-
- 
+  validates :family_name, format: { with: /\A[あ-んア-ン一-龥]+\z/, message: 'Full-width characters' }
+  validates :last_name, format: { with: /\A[あ-んア-ン一-龥]+\z/, message: 'Full-width characters' }
+  validates :family_name_reading, format: { with: /\A[ア-ン]+\z/, message: 'Full-width katakana characters' }
+  validates :last_name_reading, format: { with: /\A[ア-ン]+\z/, message: 'Full-width katakana characters' }
 end
