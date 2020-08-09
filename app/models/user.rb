33a141f5b2201c0ca9a3d3ee62d9108fa42ea490
kanahashimoto03 <hka13ba077@icloud.com>
2020-08-09 18:20:32 +0900

    userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,13 @@ class User < ApplicationRecord
 
   validates :nickname, :family_name, :last_name, :family_name_reading, :last_name_reading, :birthday, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, format: {with: PASSWORD_REGEX}
-
-  validates :family_name, :last_name, format: {with: /\A[あ-んア-ン一-龥]+\z/}
-  validates :family_name_reading, :last_name_reading, format: {with: /\A[ア-ン]+\z/}
+  validates :password, format: {with: PASSWORD_REGEX, message: "Include both letters and numbers"}
   
+  validates :family_name, format: {with: /\A[あ-んア-ン一-龥]+\z/, message: "Full-width characters"}
+  validates :last_name, format: {with: /\A[あ-んア-ン一-龥]+\z/, message: "Full-width characters"}
+  validates :family_name_reading, format: {with: /\A[ア-ン]+\z/, message: "Full-width katakana characters"}
+  validates :last_name_reading, format: {with: /\A[ア-ン]+\z/, message: "Full-width katakana characters"}
+
+
  
 end
