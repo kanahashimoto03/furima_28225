58558cd0e17dfa5574e60943a243968a0c7884e6
kanahashimoto03 <hka13ba077@icloud.com>
2020-08-17 15:04:10 +0900

    購入ページに商品表示

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -1,2 +1,12 @@
 class OrderHistoriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only:[:index, :new, :create, :destroy]
-  resources :items
+  resources :items do
+    resources :order_histories, only: [:index, :create]
+  end
 end
