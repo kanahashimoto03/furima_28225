ae12ef6589edce05a33486df79732b5be1bb71e1
kanahashimoto03 <hka13ba077@icloud.com>
2020-08-19 10:50:01 +0900

    クレジットカード機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :users, only:[:index, :new, :create, :destroy]
   resources :items do
-    resources :order_histories, only:[:index, :create]
+    resources :order_histories, only: [:index, :create]
   end
 end
